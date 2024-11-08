2c90047f9abc8dbaa336a6701cad7186d352a8e8
shadbalti2@gmail.com <shadbalti2@gmail.com>
2024-11-09 02:43:46 +0500

    fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,30 +47,6 @@ export const metadata: Metadata = {
 const RootLayout: React.FC < { children: React.ReactNode } > = ({ children }) => {
   return (
     <html lang="en">
-         <head>
-              <meta charSet="UTF-8" />
-              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-              <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-              <meta name="description" content={metadata.description} />
-              <meta name="robots" content={metadata.robots.index ? 'index' : 'noindex'} />
-              <meta name="robots" content={metadata.robots.follow ? 'follow' : 'nofollow'} />
-              <meta property="og:type" content={metadata.openGraph.type} />
-              <meta property="og:url" content={metadata.openGraph.url} />
-              <meta property="og:title" content={metadata.openGraph.title} />
-              <meta property="og:description" content={metadata.openGraph.description} />
-              <meta property="og:site_name" content={metadata.openGraph.siteName} />
-              <meta property="og:image" content={metadata.openGraph.images[0].url} />
-              <meta property="og:image:width" content={String(metadata.openGraph.images[0].width)} />
-              <meta property="og:image:height" content={String(metadata.openGraph.images[0].height)} />
-              <meta property="og:image:alt" content={metadata.openGraph.images[0].alt} />
-              <meta name="twitter:card" content={metadata.twitter.card} />
-              <meta name="twitter:title" content={metadata.twitter.title} />
-              <meta name="twitter:description" content={metadata.twitter.description} />
-              <meta name="twitter:image" content={metadata.twitter.images[0]} />
-              <link rel="icon" href="/favicon.ico" />
-              <title>{metadata.title.default}</title>
-              {/* Structured Data */}
-            </head>
       <body>
         <Navbar />
         <main>{children}</main>
