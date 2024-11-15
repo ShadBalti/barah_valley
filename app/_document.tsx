3fa74c914f93c506ce3fa7b3133560165fb6895e
Dilshad Hussain Shadbalti <shadbalti2@gmail.com>
2024-11-15 16:10:21 +0500

    Update and rename _document.js to _document.tsx

--- app/_document.tsx
+++ app/_document.tsx
@@ -1,24 +1,30 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+
+const GA_TRACKING_ID = 'G-18RPH6R102';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Google Tag (gtag.js) */}
-        <script
+        {/* Google Analytics Script */}
+        <Script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-18RPH6R102"
-        ></script>
-        <script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-18RPH6R102');
+              gtag('config', '${GA_TRACKING_ID}');
             `,
           }}
-        ></script>
+        />
       </Head>
       <body>
         <Main />
@@ -26,4 +32,4 @@ export default function Document() {
       </body>
     </Html>
   );
- }
+}
